284014d5254299170aeddd35b86c069c4f371ba0
Ugur Yilmaz <ugur.yilmaz@skao.int>
2024-02-12 13:00:34 +0000

    Add functions and bootstrap for macos

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # dotfiles
+
+## Usage
+
+1. Copy your SSH Keys
+1. Install Homebrew and git following official [docs](https://brew.sh/)
+
+    ``` bash
+    brew install git
+    ```
+
+1. Install [chezmoi](https://www.chezmoi.io/)
+
+    ``` bash
+    sh -c "$(curl -fsLS get.chezmoi.io)"
+    ```
+
+    > [!NOTE]
+    > We are not using `--init` flag with chezmoi to have more control over the setup process
+
+1. Login to applications
+
+## Customisation
+
+TODO
--- bootstrap.sh
+++ bootstrap.sh
@@ -14,6 +14,7 @@ else
     error "Failed to obtain sudo credentials."
 fi
 
+{{ if eq .chezmoi.os "darwin" }}
 info "Installing XCode command line tools..."
 if xcode-select --print-path &>/dev/null; then
     success "XCode command line tools already installed."
@@ -25,6 +26,17 @@ fi
 
 info "Installing Rosetta..."
 sudo softwareupdate --install-rosetta
+{{ else if eq .chezmoi.os "linux" }}
+info "Checking for WSL..."
+{{   if (.chezmoi.kernel.osrelease | lower | contains "microsoft") }}
+info "WSL Detected!"
+# Install common packages for WSL
+{{ else }}
+info "Not running under WSL. Skipping WSL-specific setup."
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y software-properties-common
+fi
+{{ end }}
 
 # Package control must be executed first in order for the rest to work
 ./packages/setup.sh
