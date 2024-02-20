f3b3ed88554e4e61db74a2792f47fda6943d2dc8
Ugur Yilmaz <ugur.yilmaz@skao.int>
2024-02-20 17:03:33 +0000

    customise oh-my-posh part 1

--- dot_config/fish/functions/fish_greeting.fish
+++ dot_config/fish/functions/fish_greeting.fish
@@ -1,7 +1,7 @@
 function fish_greeting
 host -t txt istheinternetonfire.com | cut -f 2 -d '"' | cowsay -f vader
-# Only run fortune ~20% of the time
-    if test (random) -lt 6553
-        fortune -a | cowsay -f fox
+# Only run fortune ~40% of the time
+    if test (random) -lt 9999
+        fortune -a | cowsay -f stegosaurus
     end
 end
