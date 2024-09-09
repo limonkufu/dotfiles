a8bb67a5d45e0ecda0bb0d6bf51c32e3eeebe63a
Ugur Yilmaz <ugur.yilmaz@skao.int>
2024-09-09 22:20:33 +0100

    delete fish_greeting as it is too slow

--- dot_config/fish/functions/fish_greeting.fish
+++ dot_config/fish/functions/fish_greeting.fish
@@ -1,7 +1,2 @@
 function fish_greeting
-host -t txt istheinternetonfire.com | cut -f 2 -d '"' | cowsay -f vader
-# Only run fortune ~40% of the time
-    if test (random) -lt 9999
-        fortune -a | cowsay -f stegosaurus
-    end
 end
